aa1d0c809041b760e92657afb2cc238143fa6c1b
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2022-08-03 16:25:10 +0200

    Comitted a minimal React class component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const appRoot = ReactDOM.createRoot(document.body);
+const rootDOM = document.getElementById('root');
+const appRoot = ReactDOM.createRoot(rootDOM);
 
-function AppComponent(props) {
-    return <h1>Hello React! My lucky number is '{ props.random }'</h1>;
+class AppComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.random = props.random;
+    }
+
+    render() {
+        return <h1>Hello React! My lucky number is '{ this.random }'</h1>;
+    }
 }
 
 setInterval(() => {
-    let randomNumber = Math.floor(Math.random() * 9999);
+    const randomNumber = Math.floor(Math.random() * 9999);
     appRoot.render( 
         <AppComponent random={ randomNumber } />
     );
