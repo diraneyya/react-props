54bee2e32e234e98a5f31627d0952748ba49cfc5
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2022-08-03 14:37:31 +0200

    Committed a proper but minimal app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const appRoot = ReactDOM.createRoot(document.getElementById('root'));
+appRoot.render( 
     <h1>Hello React!</h1>
 );
