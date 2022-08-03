1511741de9c1f8f15660d8d5f7f45e4d57fdbfc4
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2022-08-03 15:38:13 +0200

    Comitted a minimal React function component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const appRoot = ReactDOM.createRoot(document.getElementById('root'));
-appRoot.render( 
-    <h1>Hello React!</h1>
-);
+const appRoot = ReactDOM.createRoot(document.body);
+
+function AppComponent(props) {
+    return <h1>Hello React! My lucky number is '{ props.random }'</h1>;
+}
+
+setInterval(() => {
+    let randomNumber = Math.floor(Math.random() * 9999);
+    appRoot.render( 
+        <AppComponent random={ randomNumber } />
+    );
+
+}, 1000);
