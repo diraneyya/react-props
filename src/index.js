1d24adf1232a688a89ab8a433809c00ccef18c0a
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2022-08-03 16:46:28 +0200

    Added React fragment to JSX

--- src/index.js
+++ src/index.js
@@ -4,21 +4,13 @@ import ReactDOM from 'react-dom/client';
 const rootDOM = document.getElementById('root');
 const appRoot = ReactDOM.createRoot(rootDOM);
 
-class AppComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.random = props.random;
-    }
-
-    render() {
-        return <h1>Hello React! My lucky number is '{ this.random }'</h1>;
-    }
-}
-
 setInterval(() => {
     const randomNumber = Math.floor(Math.random() * 9999);
     appRoot.render( 
-        <AppComponent random={ randomNumber } />
+        <>
+            <h1>Hello React!</h1>
+            <h2>My favorite number is { randomNumber }</h2>
+        </>
     );
 
 }, 1000);
